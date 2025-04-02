b2fc3fe0d13a6c93b8dcb9bef2b7d47bdf849858
jennifer jacqueline villatoro yuca <villatoroyuca@gmail.com>
2025-04-01 22:21:33 -0500

    Eliminando carpetas para subir versiones actualizadas

---
+++ Parcial_2/hellojsnode/index.js
@@ -0,0 +1,4 @@
+console.log("¡Hola, Node.js soy jennifer !");
+console.log("Bienvenidos aqui dejo una breve explicacion ")
+console.log("Node.js es un entorno de ejecución para JavaScript el cual permite ejecutar código JavaScript fuera del navegador, lo que es ideal para crear servidores, aplicaciones web, manejar bases de datos, y mucho más")
+console.log("¡acabo de ejecutar este código, siendo asi el primer paso en Node.js!");
---
+++ Parcial_2/hellojsvite/main.js
@@ -0,0 +1,21 @@
+Window.onload=function()
+{
+//document.getElementById('btn-producto1').addEventListener('click', function()
+ //{
+  //  const descripcion1 = "aqui encontraras tops muy comodos para poder usar en todo momento de tu dia no solo para hacer ejercicio debido a su elasticidad y material fabricado.";
+    //document.getElementById('descripcion-producto').textContent = descripcion1;
+
+}//);
+
+document.getElementById('btn-producto2').addEventListener('click', function()
+{
+    const descripcion2 = "camisetas y leggins flexibles para poder conseguir tus objetivos.";
+    document.getElementById('descripcion-producto').textContent = descripcion2;
+});
+
+document.getElementById('btn-producto3').addEventListener('click', function()
+{
+    const descripcion3 = "Zapatillas de alta calidad para carreras largas, con excelente amortiguación.";
+    document.getElementById('descripcion-producto').textContent = descripcion3;
+});
+}
